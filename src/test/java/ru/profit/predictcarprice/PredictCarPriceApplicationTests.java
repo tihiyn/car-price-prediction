2e43a3d7b22b806cf882850fb89e656a395a92b9
Nikita Tikhomirov <tihomnik@yandex.ru>
2025-05-10 15:20:07 +0300

    init commit

---
+++ src/test/java/ru/profit/predictcarprice/PredictCarPriceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.profit.predictcarprice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PredictCarPriceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
